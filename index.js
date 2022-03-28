07526dd0778032c364b4ecc46366dc1b54edbc03
Max Lettenberger <maxlettenberger@gmail.com>
2022-03-28 15:03:27 -0500

    Add event listener for enter key

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ function initSearchBar() {
             alert('must input valid search term')
         }
     })
+    input.addEventListener("keyup", function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+          // Cancel the default action, if needed
+          event.preventDefault();
+          // Trigger the button element with a click
+          submit.click();
+        }
+      });
 }
 
 initSearchBar();
